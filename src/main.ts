bfd8e17e258e35ac02e77d240335dfa9aa9e1e57
Sithipol Lalee <sithipol@buildersmart.com>
2024-01-10 16:31:03 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-// import * as cookieParser from 'cookie-parser';
 import fastifyCookie from '@fastify/cookie';
 import multipart from '@fastify/multipart';
-
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
-  const port = process.env.PORT || 3001;
-
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
-    // new FastifyAdapter(),
+    new FastifyAdapter({ trustProxy: true }),
   );
-  const options = {
-    origin: [
-      'https://temppdpa.buildersmart.com',
-      'http://localhost',
-      'http://localhost:3000',
-    ],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    //preflightContinue: false,
-    credentials: true,
-    //allowedHeaders: 'Content-Type, Accept',
-  };
-  // app.register(helmet, {
-  //   contentSecurityPolicy: {
-  //     directives: {
-  //       defaultSrc: [`'self'`],
-  //       styleSrc: [`'self'`, `'unsafe-inline'`],
-  //       imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
-  //       scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
-  //     },
-  //   },
-  // });
-  // app.useStaticAssets({ root: join(__dirname, '../../fastify-file-upload') });
-  await app.register(multipart);
-  app.enableCors(options);
   app.useGlobalPipes(new ValidationPipe());
-  await app.register(fastifyCookie);
-  await app.listen(port);
+  app.register(fastifyCookie);
+  app.register(multipart);
+  // app.enableCors(options);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
